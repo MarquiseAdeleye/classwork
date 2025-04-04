4bec5c14a8f93cb0bf596537a641d700edf34432
Marquise Adeleye <marquiseadeleye@gmail.com>
2025-04-03 21:49:00 -0400

    MethodExamples

---
+++ Unit-2-Intro-To-C#/MethodsExamples/MethodsExamples/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MethodsExamples;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        /*
+         * Demonstrate the use of programmer defined methods
+         *
+         * A method is a program the processes data and usually returns a value
+         *
+         * A method starts with a method signature which idetifies the name of the method, what type of data it
+         * returns, and what data it will accept for processing
+         *
+         * Method signature: return-type Name(data it will accept)
+         *
+         * return-type == the type of data returned from the method
+         *                 use void if the method does not return data
+         * Name() == Name of the method - Method names are in PascalCase
+         *                                PascalCase - The first letter of every word is Capitalized 
+         */
+        Console.WriteLine("Hello, World!");
+    }
+}
--- Unit-2-Intro-To-C#/StringExamples/StringExamples/Program.cs
+++ Unit-2-Intro-To-C#/StringExamples/StringExamples/Program.cs
@@ -4,15 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-      
+
         /**************************************************************************************************
          * Strings in C# are a spedial data type that don't work like you think they should
          *
          * String literals are enclosed in " "
          *
+         * method() - Program that process data in an object/variable
+         *
          * A literal is something that says what it is:
          */
 
+
+        /*
         string string1 = "a";
         string string2 = "a";
         if (string1.CompareTo(string2) > 0)
@@ -23,5 +27,14 @@ class Program
         {
             Console.WriteLine("They are not equals");
         }
+        */
+
+        //Some methods that process strings
+
+        string sentence = "My name is Marquise and I am learning C#";
+        Console.WriteLine("There are " + sentence.Length + " characters in sentence.");
+
+        Console.WriteLine("Does this sentence start with My? " + sentence.StartsWith("My"));
+        Console.WriteLine("Does this sentence start with my? " + sentence.StartsWith("my"));
     }
 }
