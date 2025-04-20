4839edf6c14dcc7e5d34036f2887be169807bba0
Marquise Adeleye <marquiseadeleye@gmail.com>
2025-04-20 13:20:17 -0400

    Shopping List

--- Unit-3-Collections/Day-3-Dictionary-Example-V2/Day-2-Dictionary-Example/Program.cs
+++ Unit-3-Collections/Day-3-Dictionary-Example-V2/Day-2-Dictionary-Example/Program.cs
@@ -17,9 +17,12 @@ namespace Day_2_Dictionary_Example
             // Dictionary to hold name (key) and grade (value)
             //         key     value
             //         type    type    name      = new Dictionary<key-type, value>();
-            Dictionary<string, double> gradeBook = new Dictionary<string, double>();
+            Dictionary<string, List<double>> gradeBook = new Dictionary<string, List<double>>();
             
-            // Lets add 3 students - loop 3 times for-loop
+            // Let's add students and grades until the user indicates they are done
+            //Loop until the user satisfies a condition
+            //Replace the for-loop with a while-loop or do-while-loop
+            /*
             for (int i = 0; i < 3; i++)
             {
                 // Ask the user for the student name and grade
@@ -32,12 +35,73 @@ namespace Day_2_Dictionary_Example
                 // Dictionary[key}     = value;
                 gradeBook[studentName] = grade; // gradeBook.Add(studentName, grade) // may cause an exception
             }
-            
+            */
+//How do we want the user to let us know when the
+            bool getGrades = false;
+            string userResponse = "";
+            do
+            {
+                
+                Console.Write("Enter student name: ");
+                string studentName = Console.ReadLine(); // Get student name// Ask the user for the student name
+                Console.Write("Enter grade: ");
+                //Because the user might handle a non-numeric grade we should
+                //handle that exception
+                double grade = 0;
+                string userGrade = "";
+                List<double> gradeList = new List<double>();//Grades enter by user
+
+                try
+                {
+                    //we need to get multiple grades for each student and store in a list
+                    string response = "";
+                    while (response != "end")
+                    {
+                        response = Console.ReadLine();
+                        if (response == "end")
+                        {
+                            break;
+                        }
+                        grade = Double.Parse(response); // Get student grade
+                        gradeList.Add(grade); //Add the grades to the list
+                    }
+                   //at the end of this loop the grades list has all the grades
+
+                }
+                catch(FormatException exceptionObject)
+                {
+                    Console.WriteLine("The data you entered (" + grade + ") is not a valid number");
+                    Console.WriteLine("The data is ignored");
+                    //Skip the rest of the loop process
+                    continue;
+                }
+
+                // Add the data to our Dictionary
+                // Dictionary[key}     = value;
+                gradeBook[studentName] = gradeList; // gradeBook.Add(studentName, grade) // may cause an exception
+
+                do
+                {
+                    Console.WriteLine("Are you done? (y/n)");
+                    //Get response from user and convert to lowercase
+                    userResponse = Console.ReadLine().ToLower();
+                    
+                    if (userResponse == "y")
+                    {
+                        getGrades = true;
+                    }else
+                    {
+                        getGrades = false;
+                    }
+                    
+                } while (userResponse == "y" && userResponse == "n");
+
+            } while (getGrades == false);
             // Display the entrys in our Dictionary
             // Use a KeyValuePair type to get an entry from teh Dictionary
-            foreach (KeyValuePair<string, double> anEntry in gradeBook)
+            foreach (KeyValuePair<string, List<double>> anEntry in gradeBook)
             {
-                Console.WriteLine(gradeBook[anEntry.Key] + " has a grade of " + anEntry.Value);
+                Console.WriteLine(anEntry.Key + " has a grade of " + anEntry.Value);
             }
             
             Console.WriteLine("Please press enter to end program...");
--- Unit-3-Collections/ShoppingListLab/ShoppingListLab/Program.cs
+++ Unit-3-Collections/ShoppingListLab/ShoppingListLab/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("-----------------Start Program----------------");
+        Dictionary<string, double> menuItems = new Dictionary<string, double>();
+        menuItems.Add("Beef", 3);
+        menuItems.Add("Milk", 5);
+        menuItems.Add("Pasta", 7);
+        menuItems.Add("Eggs", 20);
+        menuItems.Add("Chicken", 3);
+        menuItems.Add("Tomato", 7);
+        menuItems.Add("Fish", 3);
+        menuItems.Add("Cheese", 12);
+        List<string> shoppingList = new List<string>();
+
+        foreach (KeyValuePair<string, double> menuItem in menuItems)
+        {
+            Console.WriteLine("Item Name: " + menuItem.Key + " - Price: $" + menuItem.Value);
+        }
+        
+        Console.WriteLine("Do your want to add an item to your shopping list? y/n ");
+        string input = Console.ReadLine();
+        
+        
+        
+        
+        Console.WriteLine("-----------------End Program------------------");
+
     }
 }
