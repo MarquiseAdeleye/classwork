d853a38ee5408755805d9db02c39f966e7a770b0
Marquise Adeleye <marquiseadeleye@gmail.com>
2025-07-02 18:32:04 -0400

    Preclass

--- Unit-7-Angular/Day-3-formExample-Checkbox/src/app/components/form-example/form-example.component.ts
+++ Unit-7-Angular/Day-3-formExample-Checkbox/src/app/components/form-example/form-example.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { throwError } from 'rxjs';
@@ -5,7 +6,8 @@ import { throwError } from 'rxjs';
 @Component({
   selector: 'form-example',  // Name used in html to reference the component <form-example>
   standalone: true,
-  imports: [FormsModule],  // Tell Angular we want Forms support for this code
+  imports: [FormsModule, CommonModule],  // Tell Angular we want Forms support for this code
+                                         // Tell Angular we are using common processign like "ngFor"
   templateUrl: './form-example.component.html',
   styleUrl: './form-example.component.css'
 })
@@ -23,9 +25,22 @@ export class FormExampleComponent {
   // Sports is specified as a checkbox it's data will be returned as an array
   // With each element in the array corresponding to a choice
   // Checkbox need to be included in the html a individual element
-  // More tomorrow
-  //sports : any[] 
-  sports = ""
+  
+  // Deifne an array to hold the check box values
+  // Each element in the array of objects with two properties
+  //
+  // name: What is displayed
+  // value: what is returned when the checkbox is checked
+  sports : any[] = [ //any type is used to make the array flexible - hold kind of data
+    {name: 'European Football', value: "Soccer"},
+    {name: 'Cricket', value: "Cricket"},
+    {name: 'America Football', value: "Football"},
+    {name: 'Baseball', value: "Baseball"},
+    {name: 'Hockey', value: "Hockey"},
+    {name: 'Other', value: "Other Sport"},
+    {name: 'None', value: "None"}
+]
+  
  // Methods to handle interactions with the html/web page
   onSubmit() { // Handle the Submit button click on the  form
     this.buttonClicked = true
@@ -33,6 +48,37 @@ export class FormExampleComponent {
     console.log(`You entered ${this.firstName} in the First Name field`)
     console.log(`You entered ${this.lastName} in the Last Name field`)
     console.log(`You entered ${this.emailAddress} in the email field`)
-    console.log(`You entered ${this.sports} as sports you like`)
+    //console.log(`You entered ${this.sports} as sports you like`)
+    //console.table(this.sports)
+
+    //Only display checkbox items that were checked
+    //Loop thorugh the checkbox array and find all the chekboxes where checked is true
+    //Call function to return all the checkboxes that were checked
+    //console.table(this.checkBoxResult) //This will call a function named checkBoxResul
+
+    //Display each item checkd on a line by itself
+    //
+    //.forEach() - an array function that will loop through an array passing each element to an arrow function
+
+    //Use checkBoxResult() function to get an array of checked items
+    // then give the array to .forEach() to display each item
+    this.checkBoxResult.forEach((aBox) => console.log(aBox.name))
+
+  }
+
+  //This function will return an array of checked box items that were checked
+  //We designate it as get because it will return something
+  get checkBoxResult(){
+  
+   //loop through the checkbox array (called sports)
+   //if an item has checked=true, save for return when done
+   //When the loop is done, return all the items where checked was true
+   //
+   // .filter() is an array funtion that will go through an array 1 element at a time
+   // using an arrow (=>) function to tell is whether the current element
+   // should be saved in a new array
+   //
+   // if the arrow (=>) returns true, the current element is saved in a new array
+   return this.sports.filter((aCheckBox) => aCheckBox.checked)
   }
 }
